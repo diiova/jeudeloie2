69218fef8b515b945954db6d7642bffcaee3dcfa
diiova <giovanni.serra@ltpdampierre.com>
2023-11-16 09:00:35 +0100

    x

--- src/main/java/giova/nni/App.java
+++ src/main/java/giova/nni/App.java
@@ -2,37 +2,36 @@ package giova.nni;
 
 import java.util.Random;
 
-
 public class App {
-    
+
     public static void main(String[] args) {
-        
+
         int caseObjectif = 20;
         int nbLancers = 5;
         Random generateur = new Random();
 
-        boolean gagne = true;
+        boolean gagne = false;
         int compteursSimulations = 0;
-    
-        while (gagne) {
-        compteursSimulations++;
-        int caseCourant = 0;
 
-        for (int i = 1; i <= nbLancers; i++) {
-            int lancer = generateur.nextInt(6) + 1;
-            caseCourant = caseCourant + lancer;
-            System.out.println("Lancer dés : vous avez fait " +caseCourant);
-    
-        }
-        if (caseCourant == caseObjectif) {
-            System.out.println("Vous avez gagné !");
-            System.out.println("il aura fallu "+ compteursSimulations + "simulations pour gagner");
-            gagne = true;
+        while (gagne == false) {
+            compteursSimulations++;
+            int caseCourant = 0;
+
+            for (int i = 1; i <= nbLancers; i++) {
+                int lancer = generateur.nextInt(6) + 1;
+                caseCourant = caseCourant + lancer;
+                System.out.println("Lancer dés : vous avez fait " + caseCourant);
+
+            }
+            if (caseCourant == caseObjectif) {
+                System.out.println("Vous avez gagné !");
+                System.out.println("il aura fallu " + compteursSimulations + "simulations pour gagner");
+                gagne = true;
             } else if (caseCourant > caseObjectif) {
-            System.out.println("Vous dépassez, vous avez perdu !");
+                System.out.println("Vous dépassez, vous avez perdu !");
             } else {
-            System.out.println("Il vous en manque pour arriver à " + caseObjectif + " !");
+                System.out.println("Il vous en manque pour arriver à " + caseObjectif + " !");
+            }
         }
     }
-}
 }
